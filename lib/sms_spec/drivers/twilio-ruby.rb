eb08dd5cc28ba75682be1b4b4a9dbf660a2da3dc
Łukasz Szyndzielorz <lukasz.szyndzielorz@thesoftwarehouse.pl>
2015-08-04 14:22:46 +0200

    Updated SmsSpec gem to be compatibile with newest version of Twilio-Ruby gem

--- lib/sms_spec/drivers/twilio-ruby.rb
+++ lib/sms_spec/drivers/twilio-ruby.rb
@@ -1,9 +1,22 @@
 module Twilio
   module REST
-    class Client
 
+    class Client
       def initialize(account_sid, auth_token)
         $account_sid = account_sid
+      end
+      def account
+        account = Account.new
+        account.class.send(:define_method, :sid, lambda { $account_sid })
+        return account
+      end
+
+
+      class Account
+        def messages
+         Messages.new
+        end
+
       end
 
       class Messages
@@ -16,24 +29,34 @@ module Twilio
           add_message SmsSpec::Message.new(:number => to, :from => from, :body => body)
         end
       end
-
-      class Sms
-        def messages
-          return Messages.new
-        end
-      end
-
-      class Account
-        def sms
-          return Sms.new
-        end
-      end
-
-      def account
-        account = Account.new
-        account.class.send(:define_method, :sid, lambda { $account_sid })
-        account
-      end
     end
+
+    #
+    # class Sms
+    #   def messages
+    #     Twilio::REST::Messages.new
+    #   end
+    # end
+
+    # class Account
+    #   def messages
+    #     Twilio::REST::Messages.new
+    #   end
+    #
+    # end
+    #
+    # class Messages
+    #   include SmsSpec::Helpers
+    #
+    #   def create(opts={})
+    #     to = opts[:to]
+    #     body = opts[:body]
+    #     from = opts[:from]
+    #     add_message SmsSpec::Message.new(:number => to, :from => from, :body => body)
+    #   end
+    # end
+    #
+
+
   end
 end
--- lib/sms_spec/version.rb
+++ lib/sms_spec/version.rb
@@ -1,3 +1,3 @@
 module SmsSpec
-  VERSION = "0.1.9"
+  VERSION = "0.1.2"
 end
